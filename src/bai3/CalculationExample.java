0c9e70e6b8770f3fd5a078bf1e91728c29a565fd
quangtuan9997 <mrt.tuan97@gmail.com>
2023-03-30 08:59:00 +0700

    first commit

---
+++ src/bai3/CalculationExample.java
@@ -0,0 +1,29 @@
+package bai3;
+
+import java.util.Scanner;
+
+public class CalculationExample {
+    public static void calc(int x,int y){
+        try {
+            int a=x+y;
+            int b=x-y;
+            int c=x*y;
+            int d=x/y;
+            System.out.println("Tổng x+y = "+a);
+            System.out.println("Hiệu x-y = "+b);
+            System.out.println("Tích x*y = "+c);
+            System.out.println("Thương x/y = "+d);
+        }catch (Exception e){
+            System.err.println("Xảy ra ngoại lệ: "+e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("nhập giá trị x:");
+        int x=scanner.nextInt();
+        System.out.println("nhập giá trị y:");
+        int y=scanner.nextInt();
+      calc(x,y);
+    }
+}
